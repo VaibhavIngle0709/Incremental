8f6daa5a7b4671d345cfaae0282472a3d16c8f66
Iamneo <techadmin@iamneo.ai>
2023-11-18 11:17:17 +0000

    Backup Commit

--- dotnetapp/Models/Player.cs
+++ dotnetapp/Models/Player.cs
@@ -20,7 +20,7 @@ namespace dotnetapp.Models
         [Range(1,int.MaxValue,ErrorMessage="Bidding amount must be greater than 0.")]
         public decimal BiddingAmount{get;set;}
 
-        public Team team{get;set;}        
+        public Team ? team{get;set;}        
 
     }
 }
